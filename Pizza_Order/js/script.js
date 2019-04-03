f1d31213e9500459ba2217baad0df97cb03b2e7d
David <dfine39@gmail.com>
2019-04-03 15:32:00 -0400

    Initial Commit

---
+++ Pizza_Order/js/script.js
@@ -0,0 +1,10 @@
+/*==========================================
+        PIZZA-BASE OPTIONS
+==========================================*/
+
+function baseDisplay(id)
+{
+        var source = document.getElementById(id);
+        document.getElementById('base_display').innerHTML = source.innerHTML;
+   
+}
